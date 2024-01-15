d858db7d91a268b28c75ebde3d9014fee99f0ad2
root <lseirina87@gmail.com>
2024-01-15 14:41:08 +0300

    Added test wait_for_db

---
+++ app/core/management/commands/wait_for_db.py
@@ -0,0 +1,8 @@
+"""
+Custom django commanf for waiting db.
+"""
+from django.core.management.base import BaseCommand
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        pass
