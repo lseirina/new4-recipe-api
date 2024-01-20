24c65cb706e97be31c075851dcea76c252f7e02e
root <lseirina87@gmail.com>
2024-01-20 11:57:58 +0300

    Added custom useradmin + tests

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -23,10 +23,24 @@ class AdminSiteTests(TestCase):
             name='TestName'
         )
 
-    def test_get_user_listed(self):
-        """Test to get a list of users."""
+    def test_users_list(self):
+        """Test that users are listed on page."""
         url = reverse('admin:core_user_changelist')
         res = self.client.get(url)
 
         self.assertContains(res, self.user.email)
         self.assertContains(res, self.user.name)
+
+    def test_edit_user_page(self):
+        """Test the edit user page works."""
+        url = reverse('admin:core_user_change', args=[self.user.id])
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
+
+    def test_create_user_page(self):
+        """Test the create user page works."""
+        url = reverse('admin:core_user_add')
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
