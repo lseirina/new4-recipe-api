c1302edd657ab9f5159ed2f736a668af99052eb8
root <lseirina87@gmail.com>
2024-02-05 22:29:51 +0300

    Added tests for update tags in recipe

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -245,6 +245,44 @@ class PrivateRecipeTests(TestCase):
             ).exists()
             self.assertTrue(exists)
 
+    def test_create_tag_on_update(self):
+        """Test creating tag when updating a recipe."""
+        recipe = create_recipe(user=self.user)
+
+        payload ={'tags': [{'name': 'Lunch'}]}
+
+        url = detail_url(recipe.id)
+        res = self.client.patch(url, payload, format='json')
 
 
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        tag_new = Tag.objects.get(user=self.user, name='Lunch')
+        self.assertIn(tag_new, recipe.tags.all())
 
+    def test_update_recipe_assign_tag(self):
+        """Test assigning existing tag  with updating a recipe."""
+        tag_breakfast = Tag.objects.create(user=self.user, name='Breakfast')
+        recipe = create_recipe(user=self.user)
+        recipe.tags.add(tag_breakfast)
+
+        tag_lunch = Tag.objects.create(user=self.user, name='Lunch')
+        payload = {'tags': [{'name': 'Lunch'}]}
+        url = detail_url(recipe.id)
+        res = self.client.patch(url, payload, format='json')
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertIn(tag_lunch, recipe.tags.all())
+        self.assertNotIn(tag_breakfast, recipe.tags.all())
+
+    def test_clear_tags(self):
+        """Test clearing tags in recipe."""
+        recipe = create_recipe(user=self.user)
+        tag = Tag.objects.create(user=self.user, name='Desert')
+        recipe.tags.add(tag)
+
+        payload = {'tags': [{'name': []}]}
+        url = detail_url(recipe.id)
+        res = self.client.update(url, payload, format='json')
+
+        self.assertequal(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(recipe.tags.count(), 0)
